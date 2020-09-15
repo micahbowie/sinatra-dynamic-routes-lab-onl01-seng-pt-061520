3c71fd92f07973cd2ac47627383ba5a677ebbabe
Micah Bowie <micahbowie20@gmail.com>
2020-09-15 01:29:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,10 +25,8 @@ class App < Sinatra::Base
     "#{@word1 @word2 @word3 @word4 @word5}"
   end
   
-  get "/say/:word1/:word2/:word3/:word4/:word5"
-    @word1, @word2, @word3, @word4, @word5 = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5] 
-    
-    "#{@word1 @word2 @word3 @word4 @word5}"
+  get "/:operation/:number1/:number2"
+    @operation = params[:operation]
   end
   
  
