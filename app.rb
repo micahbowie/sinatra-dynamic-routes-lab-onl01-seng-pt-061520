7b38e01c93a4d4a4ec933adc32e1d98ca98ff80f
Micah Bowie <micahbowie20@gmail.com>
2020-09-15 01:28:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,10 +20,9 @@ class App < Sinatra::Base
   end
   
   get "/say/:word1/:word2/:word3/:word4/:word5"
-    @num = params[:number].to_i 
-    @phrase = params[:phrase]
+    @word1, @word2, @word3, @word4, @word5 = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5] 
     
-    @phrase * @num
+    "#{@word1 @word2 @word3 @word4 @word5}"
   end
   
  
