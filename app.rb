11a32ab6e2ae51c6a271ef14a85ae36d3b2e733f
Micah Bowie <micahbowie20@gmail.com>
2020-09-15 01:41:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,46 +2,39 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
  
-  get "/reversename/:name"
+    get "/reversename/:name" do
     @name = params[:name]
     "#{@name.reverse}"
-  end 
+  end
 
-  get "/square/:number"
-    @num = params[:number].to_i 
-    "#{@num * @num}"
-  end 
-  
-  get "/say/:number/:phrase"
-    @num = params[:number].to_i 
-    @phrase = params[:phrase]
-    
-    @phrase * @num
+  get "/square/:number" do
+    @num = params[:number].to_i
+    "#{@num**2}"
   end
-  
-  get "/say/:word1/:word2/:word3/:word4/:word5"
-    @word1, @word2, @word3, @word4, @word5 = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5] 
-    
-    "#{@word1 @word2 @word3 @word4 @word5}"
+
+  get "/say/:number/:phrase" do
+    @num, @phrase = params[:number].to_i, params[:phrase]
+    "#{@phrase*@num}"
   end
-  
-  get "/:operation/:number1/:number2"
-    @num1, @num2 = params[:number1].to_i, params[:number2].to_i
-    @operation =  params[:operation]
-    
-      if @opertaion == 'add'
-        @operation = +
-      elsif @opertaion == 'subtract'
-        @operation = -
-      elsif @opertaion == 'divide'
-        @operation = /
-      else
-        @operation = *
-      end 
-    
-    "#{@num1 @operation @num2}"
-  
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @words = []
+    params.values.each {|word| @words << word}
+    @sentence = @words.join(" ")
+    "#{@sentence}."
   end
-  
- 
+
+  get "/:operation/:number1/:number2" do
+    @op, @num1, @num2 = params[:operation], params[:number1].to_i, params[:number2].to_i
+    case @op
+    when "add"
+      "#{@num1+@num2}"
+    when "subtract"
+      "#{@num1-@num2}"
+    when "multiply"
+      "#{@num1*@num2}"
+    when "divide"
+      "#{@num1/@num2}"
+    end
+  end
 end
